7c33f54d40b48a14a3fb260964fb4a35c731f2a7
DmitriyZit <adima74@gmail.com>
2023-10-11 05:50:49 +0300

    less1

--- less1.py
+++ less1.py
@@ -1,5 +1,17 @@
-a = input("Введите число")
-print(a)
-print("введите следущее")
-b = input()
-print(b)
+#a = input("Введите число")
+#print(a)
+#print("введите следущее")
+#b = input()
+#print(b)
+#print("введите следущее")
+#c = int(input())
+#print(c + 5)
+'''
+m = 6.7787887
+print(round(m, 2)) # округление до 2 знаков после запятой
+a ="dhddhffe"
+for i in a: # вывод букв по номерам
+    print(i)
+'''
+text = "jjkoddl"
+print(len(text)) # размер строки
