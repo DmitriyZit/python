2e166bae7ef0ca7d0f854988e50abad2c44e4cc8
DmitriyZit <132575123+DmitriyZit@users.noreply.github.com>
2023-10-19 08:12:37 +0300

    Update sem1.py

--- sem1.py
+++ sem1.py
@@ -4,7 +4,7 @@ n = int(input("????????? ???? ???? ? ????: "))
 m = int(input("????????? ??????????: "))
 res = m / n
 print(math.ceil(res))
-"""
+
 m = int(input("введите число: "))
 i = 0
 while  m != 0:
@@ -12,3 +12,24 @@ while  m != 0:
     m = m // 10
     i += 1
 print(i)
+"""
+import math
+n = int(input("Введитите путь авто в день: "))
+m = int(input("Введитите расстояние: "))
+res = m / n
+print(math.ceil(res))
+
+m = int(input("Введите число: "))
+i = 0
+while  m != 0:
+    print(m // 10)
+    m = m // 10
+    i += 1
+print(i)
+"""
+# високосный год
+n = int(input("Введитите год: "))
+if n % 4 == 0 and n % 100 != 0 or n % 400 == 0:
+    print("YES")
+else:
+    print("NO")
