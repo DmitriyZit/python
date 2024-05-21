b39fe39a7d3f47b1eeb088eed8669beb02c708d5
DmitriyZit <adima74@gmail.com>
2024-05-22 02:10:07 +0300

    sem5_1

--- sem5_1.py
+++ sem5_1.py
@@ -1,3 +1,4 @@
+'''
 list1 = [1, 5, 5, 7, 9, 66, 77]
 #print(len(set(list1)))
 print("Введите сдвиг")
@@ -13,3 +14,15 @@ print(list_res)
 for i in range(len(list1) - k):
     list_res.append(list1[i])
 print(list_res)
+'''
+
+list1 =  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+{"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
+
+set1 = set()
+for i in list1:
+    
+    for j in i:
+        print(j)
+        set1.add(i[j])
+print(set1)
