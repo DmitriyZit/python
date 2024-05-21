30a98e6e8979cef5672c460e8dfb571420af4d87
DmitriyZit <adima74@gmail.com>
2024-05-22 01:49:14 +0300

    sem5_1

--- sem5_1.py
+++ sem5_1.py
@@ -1,2 +1,15 @@
 list1 = [1, 5, 5, 7, 9, 66, 77]
-print(len(set(list1)))
+#print(len(set(list1)))
+print("Введите сдвиг")
+k = int(input())
+k = k % len(list1)
+
+list_res = []
+
+for i in range(k):
+    list_res.append(list1[len(list1) - 1 - i])
+print(list_res)
+
+for i in range(len(list1) - k):
+    list_res.append(list1[i])
+print(list_res)
