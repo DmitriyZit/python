5e9676c9646fbcd4cf2edd210141ce55d15f1aa2
DmitriyZit <adima74@gmail.com>
2024-05-22 05:17:34 +0300

    sem5_1

--- sem5_1.py
+++ sem5_1.py
@@ -55,20 +55,43 @@ for i in range(len(list_1)):
         print(list_1[i])
 
 
-'''
-list_1 = [1, 2, 3, 4, 5,] 
-k = 5
+
+list_1 = [13, 22, 3, 4, 5, 9, 10] 
+k = 6
+
 num = 0
-for i in range(len(list_1)): 
+
+#for i in range(len(list_1)):
+#   if list_1[i] == k:
+#        print(list_1[i])
+
+for i in range(0, len(list_1)): 
     if list_1[i] < k: 
         num = -k 
         print(num)
+        
     else: 
         num = num + 0
     if list_1[i] >= k and list_1[i] - k <= num - k: 
         num = list_1[i] 
         print(num)
+        
     elif list_1[i] <= k and num - k <= list_1[i] - k: 
         num = list_1[i] 
-        print(num)
+        
 print(num)
+
+'''
+list_1 = [1, 2, 3, 4, 5] 
+k = 5
+num = list_1[0]
+#print(num)
+for i in range(len(list_1)): 
+    if abs(list_1[i] - k) < abs(num - k): 
+        num = list_1[i] 
+        print(num)
+    else:
+        
+
+        print(num)
+        
