78eb2d05bcf9e1652a147b821858621d39d022af
DmitriyZit <adima74@gmail.com>
2024-06-14 13:40:21 +0300

    dd

--- less2.py
+++ less2.py
@@ -36,7 +36,7 @@ while i < n - 2:
  
 print("Значение этого элемента:", fib2)
 
-'''
+
 # Генератор списков
 list_1 = []
 for i in range(1, 101):
@@ -46,3 +46,19 @@ print(list_1)
 # Или
 list_1 = [i for i in range(1, 50) if i % 2 ==0]
 print(list_1)
+'''
+# Сдвиг в списке
+
+list1 = [5, 4, 6, 7, -10]
+k = int(input())
+k = k % len(list1)
+
+list_res = []
+
+for i in range(k):
+    list_res.append(list1[len(list1) - 1 - i])
+print(list_res)
+
+for i in range(len(list1) - k):
+    list_res.append(list1[i])
+print(list_res)
